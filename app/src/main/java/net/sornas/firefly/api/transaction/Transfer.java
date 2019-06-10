5070d6404f4f125306cff031f1d90e588c516cc4
Gustav Sörnäs <gustav@sornas.net>
2019-06-10 22:04:53 +0200

    Add tags

--- app/src/main/java/net/sornas/firefly/api/transaction/Transfer.java
+++ app/src/main/java/net/sornas/firefly/api/transaction/Transfer.java
@@ -3,16 +3,18 @@ package net.sornas.firefly.api.transaction;
 import net.sornas.firefly.api.account.AssetAccount;
 import net.sornas.firefly.api.budget.Budget;
 import net.sornas.firefly.api.category.Category;
+import net.sornas.firefly.api.tag.Tag;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Transfer extends Transaction {
     public Transfer(String description, double amount, LocalDateTime dateTime,
-                    AssetAccount source, AssetAccount destination, Category category, Budget budget) {
-        super(description, amount, dateTime, source, destination, category, budget);
+                    AssetAccount source, AssetAccount destination, Category category, Budget budget, List<Tag> tags) {
+        super(description, amount, dateTime, source, destination, category, budget, tags);
     }
     public Transfer(String description, double amount, String dateTime,
-                    AssetAccount source, AssetAccount destination, Category category, Budget budget) {
-        super(description, amount, dateTime, source, destination, category, budget);
+                    AssetAccount source, AssetAccount destination, Category category, Budget budget, List<Tag> tags) {
+        super(description, amount, dateTime, source, destination, category, budget, tags);
     }
 }
--- app/src/main/java/net/sornas/firefly/api/transaction/Withdrawal.java
+++ app/src/main/java/net/sornas/firefly/api/transaction/Withdrawal.java
@@ -4,16 +4,18 @@ import net.sornas.firefly.api.account.AssetAccount;
 import net.sornas.firefly.api.account.FlowAccount;
 import net.sornas.firefly.api.budget.Budget;
 import net.sornas.firefly.api.category.Category;
+import net.sornas.firefly.api.tag.Tag;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Withdrawal extends Transaction{
     public Withdrawal(String description, double amount, LocalDateTime dateTime,
-                      AssetAccount source, FlowAccount destination, Category category, Budget budget) {
-        super(description, amount, dateTime, source, destination, category, budget);
+                      AssetAccount source, FlowAccount destination, Category category, Budget budget, List<Tag> tags) {
+        super(description, amount, dateTime, source, destination, category, budget, tags);
     }
     public Withdrawal(String description, double amount, String dateTime,
-                      AssetAccount source, FlowAccount destination, Category category, Budget budget) {
-        super(description, amount, dateTime, source, destination, category, budget);
+                      AssetAccount source, FlowAccount destination, Category category, Budget budget, List<Tag> tags) {
+        super(description, amount, dateTime, source, destination, category, budget, tags);
     }
 }
