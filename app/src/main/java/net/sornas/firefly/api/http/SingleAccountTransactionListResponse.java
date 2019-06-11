47a3d1056f6e3890f016ff4c3efff436a637b5ce
Gustav Sörnäs <gustav@sornas.net>
2019-06-11 18:16:03 +0200

    Fix wrong api-response

--- app/src/main/java/net/sornas/firefly/api/http/SingleAccountTransactionListResponse.java
+++ app/src/main/java/net/sornas/firefly/api/http/SingleAccountTransactionListResponse.java
@@ -23,30 +23,26 @@ public class SingleAccountTransactionListResponse {
         private SingleAccountTransactionResponseDataAttributes attributes;
 
         private class SingleAccountTransactionResponseDataAttributes {
-            private SingleAccountTransaction transactions;  // plural men inte en lista ??
-
-            private class SingleAccountTransaction {
-                String type; // [ withdrawal, expense, deposit, transfer, opening balance, reconciliation ]
-                String date; // example: 2018-09-17T12:46:47+01:00
-                double amount;
-                String description;
-                int source_id;
-                String source_name;
-                String source_type; // [ Default account, Cash account, Asset account, Expense account, Revenue account,
-                                    // Initial balance account, Beneficiary account, Import account,
-                                    // Reconciliation account, Loan, Debt, Mortgage ]
-                int destination_id;
-                String destionation_name;
-                String destionation_type; // se source_type
-                int budget_id;
-                String budget_name;
-                String category_name;
-                int bill_id;
-                String bill_name;
-                boolean reconciled;
-                String notes;
-                String tags;  // "Array of tags. Not sure how to format it." troligen {tag1, tag2, tag3}
-            }
+            String type; // [ withdrawal, expense, deposit, transfer, opening balance, reconciliation ]
+            String date; // example: 2018-09-17T12:46:47+01:00
+            double amount;
+            String description;
+            int source_id;
+            String source_name;
+            String source_type; // [ Default account, Cash account, Asset account, Expense account, Revenue account,
+                                // Initial balance account, Beneficiary account, Import account,
+                                // Reconciliation account, Loan, Debt, Mortgage ]
+            int destination_id;
+            String destionation_name;
+            String destionation_type; // se source_type
+            int budget_id;
+            String budget_name;
+            String category_name;
+            int bill_id;
+            String bill_name;
+            boolean reconciled;
+            String notes;
+            String tags;  // "Array of tags. Not sure how to format it." troligen {tag1, tag2, tag3}
         }
     }
 
@@ -55,8 +51,8 @@ public class SingleAccountTransactionListResponse {
                 SingleAccountTransactionListResponse.class);
         List<Transaction> transactions = new LinkedList<>();
         for (SingleAccountTransactionResponseData transactionData: data) {
-            SingleAccountTransactionResponseData.SingleAccountTransactionResponseDataAttributes.SingleAccountTransaction
-                    attributes = transactionData.attributes.transactions;
+            SingleAccountTransactionResponseData.SingleAccountTransactionResponseDataAttributes
+                    attributes = transactionData.attributes;
             Transaction transaction;
             switch (attributes.type) {
                 case "withdrawal":
